bc827e07575c1a3510bb54230bbf07454d854012
DefinitelyNotJosh1 <krasnogo27@up.edu>
2025-04-14 21:50:39 -0700

    add copy.sh

---
+++ copy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Copying files to ../"
+
+cp index.apple-touch-icon.png ..
+cp index.audio.worklet.js ..
+cp index.html ..
+cp index.icon.png ..
+cp index.js ..
+cp index.pck ..
+cp index.png ..
+cp index.wasm ..
+
+echo "Done."
